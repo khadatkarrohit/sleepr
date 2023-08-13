a01345be94cba516228e33f26538a27049da334b
Rohit Khadatkar <rohit.khadatkar@rsystems.com>
2023-08-13 14:38:50 +0530

    Reservation Service

---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationsRepository } from './reservations.repository';
+
+@Injectable()
+export class ReservationsService {
+
+  constructor(
+    private readonly reservationRepository: ReservationsRepository,
+  ){}
+
+  create(createReservationDto: CreateReservationDto) {
+    return this.reservationRepository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123',
+    });
+  }
+
+  async findAll() {
+    return this.reservationRepository.find({});
+  }
+
+  // async findOne(_id: string) {
+  //  return this.reservationRepository.findOne({ _id });
+  // } 
+
+  async update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservationRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  async remove(_id: string) {
+    return this.reservationRepository.findOneAndDelete({ _id });
+  }
+}
